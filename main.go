f3c3e9c3ee37ca109f18a3eb242c10af3ff8fea0
songziming.me <songziming.me@bytedance.com>
2023-03-10 15:45:07 +0800

    internalcall

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
 	"strconv"
 	"time"
 
@@ -21,36 +25,68 @@ type Item struct {
 	Price float32 `json:"price"`
 }
 
+type CountReq struct {
+	Num int `json:"num"`
+}
+
 func main() {
-	http.HandleFunc("/api/v1/add_and_get_count", AddAndGetCount)
-	http.HandleFunc("/api/v1/call_another_service", CallAnotherService)
+	http.HandleFunc("/api/v1/get_count", GetCount)
+	http.HandleFunc("/api/v1/post_count", PostCount)
+	http.HandleFunc("/api/v1/internal_call", InternalCall)
 	http.HandleFunc("/v1/ping", Ping)
 	http.HandleFunc("/api/v1/ping", Ping2)
 	http.ListenAndServe(":8000", nil)
 }
 
-func AddAndGetCount(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("AddAndGetCount start")
+func GetCount(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("GetCount start")
 	req.ParseForm()
 	numStr := req.Form.Get("num")
 	num, _ := strconv.Atoi(numStr)
 	num++
-	fmt.Printf("AddAndGetCount return num: %d\n", num)
+	fmt.Printf("GetCount return num: %d\n", num)
 	w.Write([]byte(strconv.Itoa(num)))
 }
 
-func CallAnotherService(w http.ResponseWriter, req *http.Request) {
-	serviceID := req.Header.Get("X-SERVICE-ID")
-	domain := fmt.Sprintf("%s.dycloud.service", serviceID)
+func PostCount(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("PostCount start")
+	body, _ := ioutil.ReadAll(req.Body)
+	countReq := new(CountReq)
+	err := json.Unmarshal(body, countReq)
+	if err != nil {
+		fmt.Printf("json.Unmarshal failed, err: %+v", err)
+		w.Write([]byte(fmt.Sprintf("json.Unmarshal failed, err: %+v", err)))
+		return
+	}
+	num := countReq.Num
+	num++
+	fmt.Printf("PostCount return num: %d\n", num)
+	w.Write([]byte(strconv.Itoa(num)))
+}
+
+func InternalCall(w http.ResponseWriter, req *http.Request) {
+	toServiceID := req.Header.Get("X-SERVICE-ID")
+	method := req.Header.Get("X-SERVICE-METHOD")
+	v := req.Header.Get("X-SERVICE-VALUE")
 	uri := req.Header.Get("X-SERVICE-URI")
-	url := fmt.Sprintf("%s%s", domain, uri)
-	fmt.Printf("url: %s\n", url)
-	resp, err := http.Get(url)
+	var resp *http.Response
+	var err error
+	if method == "GET" {
+		resp, err = InternalCallGet(uri, toServiceID, map[string]string{"num": v}, map[string]string{"X-Test-Header1": "testHeader1"})
+	} else if method == "POST" {
+		num, _ := strconv.Atoi(v)
+		body, _ := json.Marshal(CountReq{Num: num})
+		resp, err = InternalCallPost(uri, toServiceID, bytes.NewBuffer(body), map[string]string{"X-Test-Header1": "testHeader1"})
+	} else {
+		w.Write([]byte(fmt.Sprintf("error: %+v", fmt.Errorf("invalid method: %s", method))))
+	}
+
 	if err != nil {
 		w.Write([]byte(fmt.Sprintf("error: %+v", err)))
 		fmt.Printf("error: %+v\n", err)
 		return
 	}
+	defer resp.Body.Close()
 	s, _ := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		w.Write([]byte(fmt.Sprintf("error: %+v", err)))
@@ -58,6 +94,45 @@ func CallAnotherService(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("resp:%+v, write: %s\n", resp, s)
 	w.Write([]byte(s))
 	fmt.Println("CallAnotherService")
+}
+
+func InternalCallGet(uri, toServiceID string, paramMap map[string]string, headers map[string]string) (*http.Response, error) {
+	fromServiceID := os.Getenv("service_id")
+	urlValue := &url.URL{
+		Scheme: "http",
+		Host:   fmt.Sprintf("%s-%s.dycloud.service", fromServiceID, os.Getenv("service_id")),
+		Path:   uri,
+	}
+	params := url.Values{}
+	for k, v := range paramMap {
+		params.Set(k, v)
+	}
+	urlValue.RawQuery = params.Encode()
+	urlPath := urlValue.String()
+	fmt.Println(urlPath)
+	req, err := http.NewRequest("GET", urlPath, nil)
+	if err != nil {
+		fmt.Printf("http.NewRequest failed, err: %+v\n", err)
+	}
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	fmt.Printf("req: %+v\n", req)
+	return http.DefaultClient.Do(req)
+}
+
+func InternalCallPost(uri, toServiceID string, body io.Reader, headers map[string]string) (*http.Response, error) {
+	fromServiceID := os.Getenv("service_id")
+	urlPath := fmt.Sprintf("http://%s-%s.dycloud.service%s", fromServiceID, os.Getenv("service_id"), uri)
+	req, err := http.NewRequest("POST", urlPath, body)
+	if err != nil {
+		fmt.Printf("http.NewRequest failed, err: %+v\n", err)
+	}
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	fmt.Printf("req: %+v\n", req)
+	return http.DefaultClient.Do(req)
 }
 
 func Ping(w http.ResponseWriter, req *http.Request) {
